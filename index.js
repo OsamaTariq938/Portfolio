cbc287a8d16e4f1d5be052208f532d1e5d4c3752
osamatariq938 <osamatraiq938@gmail.com>
2021-04-08 16:33:05 +0500

    First Phase of Portfolio

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('added');
+let pic = document.getElementById('pic');
+let fadein = document.getElementById('fadein').style.display = 'none';
+fadein = document.getElementById('fadein')
+document.addEventListener('scroll', () => {
+    
+    $("#fadein").fadeIn(4000);
+    
+})
